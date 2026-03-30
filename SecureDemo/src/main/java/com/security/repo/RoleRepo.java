d7a341b2b3bbb7f1e007351b4b9b0139f92f3ddf
Sunilashokbhoi <sunilbhoi606@gmail.com>
2026-03-30 17:55:16 +0530

    UC-3-CreateUerServiceImpl

---
+++ SecureDemo/src/main/java/com/security/repo/RoleRepo.java
@@ -0,0 +1,11 @@
+package com.security.repo;
+
+import com.security.entity.AppRole;
+import com.security.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepo extends JpaRepository<Role, Long> {
+    Optional<Role> findByRoleName(AppRole appRole);
+}
